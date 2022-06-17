06ceae532384a9fd6ea5b2e9ff55e93270c547ef
Don Svederus <svederus@gmail.com>
2022-06-16 23:11:46 -0600

    0.80.1 js text and animation

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+const header = document.getElementById("header");
+const title = document.getElementById("title");
+const excerpt = document.getElementById("excerpt");
+const profile_img = document.getElementById("profile_img");
+const name = document.getElementById("name");
+const date = document.getElementById("date");
 
+const animated_bgs = document.querySelectorAll(".animated-bg");
+const animated_bg_texts = document.querySelectorAll(".animated-bg-text");
+
+setTimeout(getData, 2500);
+
+function getData() {
+  header.innerHTML =
+    '<img src="https://images.unsplash.com/photo-1650169075825-1ee9efe6e66a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2502&q=80" alt=""/>';
+  title.innerHTML = "Lorem ipsum dolor sit amet.";
+  excerpt.innerHTML =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, nisi.";
+  profile_img.innerHTML =
+    '<img src="https://randomuser.me/api/portraits/men/22.jpg" alt="" />';
+  name.innerHTML = "John Doe";
+  date.innerHTML = "June 15, 2022";
+
+  animated_bgs.forEach((bg) => bg.classList.remove("animated-bg"));
+  animated_bg_texts.forEach((bg) => bg.classList.remove("animated-bg-text"));
+}
